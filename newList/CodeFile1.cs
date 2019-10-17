0269e8f9babf10d9185815d9d0661ac3c4fa6a3e
tonnac <tonnac@ilati.com>
2019-10-17 09:56:59 +0900

    10/17

---
+++ newList/CodeFile1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> ee = new List<int>();
+
+        int[] aa = new int[4];
+
+    }
+}
